36740c7992282ab2c0dd40c502aae181d2d2c350
Kaa <>
2023-03-10 21:07:27 +0100

    Added ability to format ext4 as rootfs

--- src/disks/filesystem/mod.rs
+++ src/disks/filesystem/mod.rs
@@ -1,5 +1,19 @@
 use crate::prelude::*;
 
+mod ext4;
+
 pub fn configure(c: Config) -> Config {
+    info!("Configuring the filesystem");
+
+    let root_volume: String = get_value(&c, "disk.root_volume");
+    let fs: String = get_value(&c, "filesystem.type");
+
+    if fs == "ext4" {
+        ext4::format(&root_volume);
+    } else if fs == "btrfs" {
+        ()
+    } else {
+        Error::Config("Improper filesystem supplied in config".into()).handle()
+    }
     c
 }
