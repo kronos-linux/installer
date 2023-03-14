d972fb4c7009fcc4720520aa32d5d297efc2a655
Kaa <>
2023-03-14 16:59:10 +0100

    Added structure to the system module

--- src/system/mod.rs
+++ src/system/mod.rs
@@ -1,5 +1,29 @@
 use crate::prelude::*;
 
+mod fstab;
+mod locale;
+mod mirrors;
+mod networking;
+mod portage;
+mod sysutils;
+mod update;
+
 pub fn configure(c: Config) -> Config {
+    fstab::configure(&c);
+
+    portage::configure();
+
+    locale::configure();
+
+    mirrors::configure();
+
+    sysutils::install();
+
+    sysutils::configure();
+
+    networking::configure();
+
+    update::configure();
+
     c
 }
