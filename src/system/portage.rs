ab9817c0ea9ab61be5cdb47c77ba64e5936bdda7
Kaa <>
2023-03-14 19:44:35 +0100

    Added downloading of ebuild repo

--- src/system/portage.rs
+++ src/system/portage.rs
@@ -1,3 +1,15 @@
 use crate::prelude::*;
 
-pub fn configure() {}
+pub fn configure() {
+    info!("Configuring portage");
+    sync_repo();
+}
+
+fn sync_repo() {
+    shrun(&ShellCommand::new("emerge-webrsync"));
+
+    match ShellCommand::new("emerge").args(["--sync", "-q"]).run() {
+        Ok(_) => debug!("Synced repository"),
+        Err(_) => warn!("Failed to sync repository; Ignoring..."),
+    }
+}
